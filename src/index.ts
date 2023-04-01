5e8b4b2a742ed116893635e02a598dc6a94b086d
lucifer-Hell <luciferhell58@gmail.com>
2023-04-01 16:54:18 +0530

    e

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,21 @@ import { OpenAI } from "langchain";
 import { initializeAgentExecutor } from "langchain/agents";
 import { SerpAPI, Calculator, DynamicTool } from "langchain/tools";
 const model = new OpenAI({ temperature: 0 });
+const now = new Date();
+const hours = now.getHours() % 12 || 12;
+const minutes = now.getMinutes().toString().padStart(2, "0");
+const seconds = now.getSeconds().toString().padStart(2, "0");
+const amPm = now.getHours() >= 12 ? "PM" : "AM";
+const currentTime = `${hours}:${minutes}:${seconds} ${amPm}`;
+// console.log(currentTime);
+
 const tools = [new DynamicTool({
   name: "timeCheck",
   description:
     "use this tool more then normal whenever user is asking for current time ",
   func: async () =>{
-     return "current time is 12:30 pm"
+    
+     return currentTime
     } ,
 }),new SerpAPI(), new Calculator()];
 
